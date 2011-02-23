adcc1e185c0b5bcfe7a5628ad4edd48a3faf9629
José Valim <jose.valim@gmail.com>
2011-02-23 15:18:29 +0100

    Ensure files are removed on update for ActiveRecord.

--- lib/carrierwave/orm/activerecord.rb
+++ lib/carrierwave/orm/activerecord.rb
@@ -24,10 +24,27 @@ module CarrierWave
       validates_integrity_of column if uploader_option(column.to_sym, :validate_integrity)
       validates_processing_of column if uploader_option(column.to_sym, :validate_processing)
 
-      after_save "store_#{column}!"
-      before_save "write_#{column}_identifier"
-      after_destroy "remove_#{column}!"
+      # Use symbols because strings are evaluated instead of dispatched
+      after_save :"store_#{column}!", :"remove_outdated_#{column}!"
+      before_save :"write_#{column}_identifier"
+      after_destroy :"remove_#{column}!"
 
+      class_eval <<-RUBY, __FILE__, __LINE__+1
+        def #{column}=(new_file)
+          #{column}_will_change!
+          super
+        end
+
+        # Remove outdated image if the previous upload had a file stored and the
+        # new upload has nothing stored OR stored the file in a different path.
+        def remove_outdated_#{column}!
+          previous, current = #{column}_change
+          if previous.respond_to?(:stored?) && previous.stored?
+            current_stored = current.respond_to?(:stored?) && current.stored?
+            previous.remove! if !current_stored || previous.store_path != current.store_path
+          end
+        end
+      RUBY
     end
 
   end # ActiveRecord
